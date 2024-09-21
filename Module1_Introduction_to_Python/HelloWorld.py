f4d8701f3b22ae345b00d2cdadbf1aa5e2084a0a
Devid Mazeeta Gade Francis <gdevid@athenahealth.com>
2024-09-21 18:19:00 +0530

    Initial Commit of the Project

---
+++ Module1_Introduction_to_Python/HelloWorld.py
@@ -0,0 +1,13 @@
+# Helo world in JAVA
+
+# public class Demo {
+#     public static void main (String[] args) {
+#         System.out.println("Hello World");
+#     }
+# }
+#
+# o/p:
+# Hello World
+
+# Helo world in Python
+print("Hello World")
