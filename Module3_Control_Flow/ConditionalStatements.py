2262315d89e7ac2fe6db83ca738beefde090a3be
Devid Mazeeta Gade Francis <gdevid@athenahealth.com>
2024-09-28 18:24:48 +0530

    Control flow

---
+++ Module3_Control_Flow/ConditionalStatements.py
@@ -0,0 +1,12 @@
+# Syntax:
+# if condition:
+#     code lines
+
+name = input("Enter the name: ")
+
+if len(name) == 5: # Ednue => True, TCS => False
+    print("Length of the given name is 5")
+
+# Not
+if not len(name) == 5:
+    print("Length of the given name is not 5")
---
+++ Module3_Control_Flow/NestedConditions.py
@@ -0,0 +1,14 @@
+fruit = "Kiwi"
+
+if len(fruit) > 4:
+    if "e" in fruit:
+        print("Valid fruit")
+    else:
+        print("Invalid fruit")
+else:
+    if "p" in fruit:
+        print("else => if")
+    elif "w" in fruit:
+        print("else => elif")
+    else:
+        print("else => else")
