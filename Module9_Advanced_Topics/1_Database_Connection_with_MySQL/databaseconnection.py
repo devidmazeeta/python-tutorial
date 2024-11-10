72f5d33cd10b9029d4ed88431388fc7fd8504164
Devid Mazeeta Gade Francis <gdevid@athenahealth.com>
2024-11-10 09:37:16 +0530

    Update DB program and Final Project Notes

--- Module9_Advanced_Topics/1_Database_Connection_with_MySQL/databaseconnection.py
+++ Module9_Advanced_Topics/1_Database_Connection_with_MySQL/databaseconnection.py
@@ -1,17 +1,21 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",  # Default XAMPP MySQL username
-    password="",  # Default XAMPP MySQL password
-    database="ednue"  # Replace with your database name
+  host="localhost",
+  user="root",  # Default XAMPP MySQL username
+  password="",  # Default XAMPP MySQL password
+  database="ednue"
 )
 
-mycursor = mydb.cursor()
+mycursor1 = mydb.cursor()
+mycursor1.execute("INSERT INTO student_details(id, name, coursename, mobilenumber) VALUES (3,'Alex','C++',454545454)")  # Replace with your table name
+mydb.commit()
 
-mycursor.execute("SELECT * FROM your_table_name")  # Replace with your table name
-
-myresult = mycursor.fetchall()
+mycursor2 = mydb.cursor()
+mycursor2.execute("SELECT * FROM student_details")  # Replace with your table name
+myresult = mycursor2.fetchall()
 
 for x in myresult:
-    print(x)
+  print(x)
+
+mydb.close()
